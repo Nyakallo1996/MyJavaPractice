a36b1a0818f431803c6c50cd98499075fca6c1d2
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2025-09-17 21:22:47 +0200

    small changes

---
+++ src/Object_Oriented_Thinking/TestLoanClass.java
@@ -0,0 +1,20 @@
+package Object_Oriented_Thinking;
+
+import java.util.Scanner;
+
+public class TestLoanClass {
+    public static void main(String[] args) {
+
+        //Create a scanner
+        Scanner input = new Scanner(System.in);
+
+        // Enter annual interest rate
+        System.out.print(
+                "Enter annual interest rate, for example, 8.25: "
+        );
+
+        double annualInterestRate = input.nextDouble();
+
+
+    }
+}
