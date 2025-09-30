ca024158901d732ef417ca0abad4d4cdcf8e825d
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2025-09-30 21:58:19 +0200

    small changes

---
+++ src/Arithmetic/Main.java
@@ -0,0 +1,20 @@
+package Arithmetic;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        z = x % y;
+
+        System.out.println(z);
+    }
+}
