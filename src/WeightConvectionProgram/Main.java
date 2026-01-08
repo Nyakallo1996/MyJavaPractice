48e384621eeda8fe230e89b1069298cbd2148dd0
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-01-08 22:26:09 +0200

    small changes

--- src/WeightConvectionProgram/Main.java
+++ src/WeightConvectionProgram/Main.java
@@ -6,33 +6,55 @@ public class Main {
     public static void main(String[] args) {
         // WEIGHT CONVERSION PROGRAM
 
-        // Declare VARIABLES
 
-        // welcome message
 
-        // prompt for user choice
 
-        // option 1 to convert lbs to kgs
 
-        // option 2 convert kgs to lbs
+
+
+
+
+
 
         // else print not valid choice
 
         Scanner scanner = new Scanner(System.in);
 
+        // Declare VARIABLES
         double weight;
         double newWeight;
         int choice;
 
+
+
+        // welcome message
         System.out.println("Weight Convection Program");
+
+        // prompt for user choice
         System.out.println("1: Convert lbs to kgs");
         System.out.println("2: Convert kgs to lbs");
 
         System.out.println("Choose an option: ");
         choice = scanner.nextInt();
 
+        // option 1 to convert lbs to kgs
+
         if(choice == 1){
             System.out.println("Enter weight in lbs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 0.453592;
+            System.out.printf("The new weight in kgs is: %.2f", newWeight);
+        }
+        // option 2 convert kgs to lbs
+        else if(choice == 2){
+            System.out.println("Enter weight in kgs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 2.20462;
+            System.out.printf("The new weight in lbs is: %.2f", newWeight);
+        }
+        // else print not valid choice
+        else{
+            System.out.println("Invalid option");
         }
     }
 }
