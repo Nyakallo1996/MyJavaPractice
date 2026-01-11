f88c8ea7bb86571774b91deebb054f0fa4c8b8c9
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-01-11 21:57:45 +0200

    small changes

---
+++ src/While_loop/Main.java
@@ -0,0 +1,48 @@
+package While_loop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // While loop = repeat some code forever
+        //              while some condition remains true
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*String name = "";
+
+        while(name.isEmpty()){
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.println("Hello " + name);
+
+        String response = "";
+
+        while(!response.equals("Q")){
+            System.out.println("You are playing a game ");
+            System.out.println("Press Q to quit: ");
+            response = scanner.next().toUpperCase();
+
+        }
+
+        System.out.println("You have quit the game");*/
+
+        int age = 0;
+
+        System.out.println("Enter your age: ");
+        age = scanner.nextInt();
+
+        while(age < 0 || age == 0){
+            System.out.println("Your age cannot be negative or zero");
+            System.out.println("Enter your age: ");
+            age = scanner.nextInt();
+        }
+
+        System.out.println("Your age is " + age);
+
+        scanner.close();
+    }
+}
