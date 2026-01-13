a5cb21f56a79014da1e5df3f4ffec450ae3677ff
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-01-13 10:37:28 +0200

    small changes

---
+++ src/Nested_Loops/Main.java
@@ -0,0 +1,15 @@
+package Nested_Loops;
+
+public class Main {
+    public static void main(String[] args) {
+        // nested loop = A loop inside another loop
+        //               used often with matrices or DS&A
+
+        for(int i = 1; i <= 3; i++){
+            for(int j = 1; j <= 9; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
