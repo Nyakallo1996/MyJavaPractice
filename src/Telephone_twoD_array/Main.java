98720ca854fe5710365a4f6b688ba92988a9dd29
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-01-14 23:14:40 +0200

    2D arrays

---
+++ src/Telephone_twoD_array/Main.java
@@ -0,0 +1,18 @@
+package Telephone_twoD_array;
+
+public class Main {
+    public static void main(String[] args){
+
+        char[][] telephone = {{'1', '2', '3'},
+                              {'4', '5', '6'},
+                              {'7', '8', '9'},
+                              {'*', '0', '#'}};
+
+        for(char[] row : telephone){
+            for(char number : row){
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
+    }
+}
