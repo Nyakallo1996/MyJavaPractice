937aac4abcc2d6611e34f1982dd9ca56f53dcc2a
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-01-31 21:46:47 +0200

    Inheretance

---
+++ src/Inheritance/Animal.java
@@ -0,0 +1,14 @@
+package Inheritance;
+
+public class Animal {
+
+    boolean isAlive;
+
+    Animal(){
+        isAlive = true;
+    }
+
+    void eat(){
+        System.out.println("The animal is eating");
+    }
+}
---
+++ src/Inheritance/Main.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Inheritance = one class inherits the attributes and methods
+        //              from another class.
+        //              child <- Parent
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        System.out.println(dog.isAlive);
+        cat.eat();
+    }
+}
