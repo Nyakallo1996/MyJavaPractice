eb9fc8fc7256377f95c6fa301d1b27f6dbd1b332
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-02-03 22:45:42 +0200

    Composition

---
+++ src/Composition/Main.java
@@ -0,0 +1,11 @@
+package Composition;
+
+public class Main {
+    public static void main(String[] args){
+        // Composition = Represents a "part-of" relationships between objects.
+        //               For example, an Engine is "part of" a Car.
+        //               Allows complex objects to be constructed from smaller objects.
+
+        Car car = new Car("Hilux", 2014, "GD6");
+    }
+}
