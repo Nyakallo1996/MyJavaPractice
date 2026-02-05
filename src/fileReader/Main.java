4d4b399d2064c35595f9b5b6276afd80aa4b40a5
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2026-02-05 11:08:29 +0200

    file reading

---
+++ src/fileReader/Main.java
@@ -0,0 +1,35 @@
+package fileReader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args){
+
+        // How to read a file using java (3 popular options)
+
+        // BufferedReader + FileReader: Best for reading text files line by line
+        // FileInputStream: Best for binary files (e.g. , images, audio files)
+        // RandomAccessFile: Best for read/write specific portions of a large file
+
+        String filePath = "C:\\Users\\Lekgo\\OneDrive\\Desktop\\test.txt";
+
+
+
+        try ( BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("could not locate file");
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong");
+        }
+
+    }
+}
